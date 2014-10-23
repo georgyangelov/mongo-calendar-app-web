56f8251959b48d4a0d1c516303488ec3a55b3545
Georgy Angelov <georgyangelov@gmail.com>
2014-10-23 21:36:41 +0300

    Show list with tasks for each day, with color-coded borders

--- app/controllers/day_panel.js
+++ app/controllers/day_panel.js
@@ -10,4 +10,12 @@ app.controller('DayPanelController', ['$scope', function($scope) {
 	$scope.cancel_add_task = function() {
 		$scope.add_task_state = false;
 	};
+
+	$scope.tasks = [
+		{title: 'MongoDB driver demo application', text: 'Create a frontend for the MongoDB drivers demo applications.', priority: 0},
+		{title: 'MongoDB driver demo application', text: 'Create a frontend for the MongoDB drivers demo applications.', priority: 1},
+		{title: 'MongoDB driver demo application', text: 'Create a frontend for the MongoDB drivers demo applications.', priority: 2},
+		{title: 'MongoDB driver demo application', text: 'Create a frontend for the MongoDB drivers demo applications.', priority: 3},
+		{title: 'MongoDB driver demo application', text: 'Create a frontend for the MongoDB drivers demo applications.', priority: 0}
+	];
 }]);
